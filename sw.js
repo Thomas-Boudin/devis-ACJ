dede6eb43b96b41214b2abd99e0cd4f57e2451a0
Thomas-Boudin <thomas.acjservices@gmail.com>
2026-01-25 11:10:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
-const CACHE = 'devis-acj-v2';
-self.addEventListener('install', evt=>{
-  evt.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-    './','devis-mobile-v2.html','manifest.json','icon-192.png','icon-512.png'
-  ])));
+
+const CACHE = 'devis-acj-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './sw.js'
+];
+
+self.addEventListener('install', (evt) => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('fetch', evt=>{
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.method !== 'GET') return;
+
   evt.respondWith(
-    caches.match(evt.request).then(r=>r||fetch(evt.request))
+    caches.match(evt.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(evt.request);
+    })
   );
 });
